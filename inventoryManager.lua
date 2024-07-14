dc69d53232d8b94fc39059015dd19ecdbefbeb15
Yuri Donato <102886538+YuriDonato@users.noreply.github.com>
2024-07-14 01:21:17 -0300

    Diorito send to help building

--- inventoryManager.lua
+++ inventoryManager.lua
@@ -1,20 +1,23 @@
 local chatbot = peripheral.wrap("back")
 
 -- Wrapping the Inventory Manager peripheral
-local manager1 = peripheral.find("up")
-local manager2 = peripheral.find("left")
+-- local manager1 = peripheral.wrap("up")
+-- local manager2 = peripheral.wrap("left")
+
+local managers = { peripheral.find("inventoryManager") }
+
+local manager1 = managers[1]
+local manager2 = managers[2]
 
 -- Function to receive blocks
 function receiveDioritoPolido(username)
     if(username == "TiozaoDosGames") then
         local freeSlot = manager1.getFreeSlot()
-        manager1.addItemToPlayer("left", {name="minecraft:cobblestone", toSlot=freeSlot, count=64})
-    elif(username == "HeadHSM") then
+        manager1.addItemToPlayer("left", {name="minecraft:polished_diorite", toSlot=36, count=64})
+    end
+    if(username == "HeadHSM") then
         local freeSlot = manager2.getFreeSlot()
-        manager2.addItemToPlayer("up", {name="minecraft:cobblestone", toSlot=freeSlot, count=64})
-    else
-        print("Username not found")
-        chatbot.sendMessage("Voce nao eh um dos donos do Kybe", "GLADOS", "<>", "&b")
+        manager2.addItemToPlayer("up", {name="minecraft:polished_diorite", toSlot=freeSlot, count=64})
     end
 end
 
--- startup.lua
+++ startup.lua
@@ -1,5 +1,7 @@
 local main = require "chatBot"
 
 while true do
+    term.clear()
+    term.write("Sistema rodando...")
     main.start()
 end

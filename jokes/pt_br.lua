cc74578ba5303bf7e353fb6d0854fad5d57dbdc4
Yuri Donato <102886538+YuriDonato@users.noreply.github.com>
2024-07-14 11:08:04 -0300

    New Jokes

--- jokes/pt_br.lua
+++ jokes/pt_br.lua
@@ -1,10 +1,25 @@
 -- Table of jokes
 jokes = {
     "Por que o livro de matematica se suicidou? Porque tinha muitos problemas.",
-    "Por que a vaca foi para o espaco? Para se encontrar com o vacuo!",
+    "Por que a vaca foi para o espaco? Para se encontrar com o vazio!",
     "O que o pato disse para a pata? Vem qua!",
     "Qual eh o rei dos queijos? O reiqueijao.",
-    "Qual eh o cumulo do desperdicio? Passar um trote para o orelhao."
+    "Qual eh o cumulo do desperdicio? Passar um trote para o orelhao.",
+    "Por que o programador foi preso? Porque deletou evidencias!",
+    "Qual eh a fruta preferida do vampiro? O sangue laranja!",
+    "Por que a lua fica triste? Porque o sol vai embora!",
+    "O que o zero disse para o oito? Oi, cinto novo!",
+    "Qual eh o animal mais antigo? A zebra, porque esta em preto e branco!",
+    "Por que o computador aposentou-se? Porque estava com dor de mouse!",
+    "Por que a pomba foi para o psiquiatra? Porque estava com um problema de vozes!",
+    "Qual eh a coisa mais pesada do mundo? O peso!",
+    "Por que o computador ficou triste? Porque perdeu o processador!",
+    "Qual eh a coisa mais leve do mundo? O vazio!",
+    "Por que o homem sentou-se? Porque estava cansado dos seus pés!",
+    "Qual eh o animal que mais dorme? O gato, porque dorme muito no dia!",
+    "Por que o homem foi para o hospital? Porque estava com uma dor de cabeça!",
+    "Qual eh a coisa mais pequena do mundo? A letra 'a'!",
+    "Por que o computador ficou com dor de cabeça? Porque estava com um bug!"
 }
 
 return {
